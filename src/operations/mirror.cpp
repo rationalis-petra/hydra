6f8f7cd3c6188ba160ffc1456b9a61ae04a17ac7
Connor Fynn Redfern <connor.redfern@optusnet.com.au>
2021-05-02 23:11:02 +0100

    Fixed set-parent

--- src/operations/mirror.cpp
+++ src/operations/mirror.cpp
@@ -11,35 +11,35 @@ using namespace expr;
 using namespace op;
 using namespace interp;
 
-#include <iostream>
 Object *op_get_slots(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr  = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   return mirr->reflectee->get_slots(r, s);
 }
 
 Object *op_get_slot_val(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   return mirr->reflectee->get_slot_value(sym, r, s);
 }
 
 Object *op_set_slot_val(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   args.pop_front();
-  Object* val = get_inbuilt<Object*>(args.front());
+  Object *val = get_inbuilt<Object *>(args.front());
   return mirr->reflectee->set_slot_value(sym, val, r, s);
 }
 
-Object *op_set_slot_parent(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+Object *op_set_slot_parent(list<Object *> args, LocalRuntime &r,
+                           LexicalScope &s) {
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   mirr->reflectee->parents.insert(sym);
   args.pop_front();
-  Object* bl = args.front();
+  Object *bl = args.front();
 
   if (bl->null()) {
     mirr->reflectee->parents.erase(sym);
@@ -51,39 +51,39 @@ Object *op_set_slot_parent(list<Object *> args, LocalRuntime &r, LexicalScope &s
 }
 
 Object *op_delete_slot(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   return mirr->reflectee->delete_slot(sym, r, s);
 }
 
 Object *op_get_meta(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr  = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   return mirr->reflectee->get_meta(r, s);
 }
 Object *op_get_meta_val(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   return mirr->reflectee->get_meta_value(sym, r, s);
 }
 Object *op_set_meta_val(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   args.pop_front();
-  Object* val = get_inbuilt<Object*>(args.front());
+  Object *val = get_inbuilt<Object *>(args.front());
   return mirr->reflectee->set_meta_value(sym, val, r, s);
 }
 Object *op_delete_meta(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Mirror* mirr = get_inbuilt<Mirror*>(args.front());
+  Mirror *mirr = get_inbuilt<Mirror *>(args.front());
   args.pop_front();
-  Symbol* sym = get_inbuilt<Symbol*>(args.front());
+  Symbol *sym = get_inbuilt<Symbol *>(args.front());
   return mirr->reflectee->delete_meta(sym, r, s);
 }
 
 Object *op_get_mirror(list<Object *> args, LocalRuntime &r, LexicalScope &s) {
-  Object* obj = get_inbuilt<Object*>(args.front());
+  Object *obj = get_inbuilt<Object *>(args.front());
   return obj->reflect(r, s);
 }
 
@@ -104,29 +104,36 @@ void op::initialize_mirror() {
   get_slots = new GenericFn;
   get_slots->type = type::Fn::with_args({type::mirror_type});
   get_slot_val = new GenericFn;
-  get_slot_val->type = type::Fn::with_args({type::mirror_type, type::symbol_type});
+  get_slot_val->type =
+      type::Fn::with_args({type::mirror_type, type::symbol_type});
   set_slot_val = new GenericFn;
-  set_slot_val->type = type::Fn::with_args({type::mirror_type, type::symbol_type, new type::Any});
+  set_slot_val->type = type::Fn::with_args(
+      {type::mirror_type, type::symbol_type, new type::Any});
   set_slot_parent = new GenericFn;
+  set_slot_parent->type = type::Fn::with_args(
+      {type::mirror_type, type::symbol_type, new type::Any});
   delete_slot = new GenericFn;
-  delete_slot->type = type::Fn::with_args({type::mirror_type, type::symbol_type});
+  delete_slot->type =
+      type::Fn::with_args({type::mirror_type, type::symbol_type});
 
   get_meta = new GenericFn;
   get_meta->type = type::Fn::with_args({type::mirror_type});
   get_meta_val = new GenericFn;
-  get_meta_val->type = type::Fn::with_args({type::mirror_type, type::symbol_type});
+  get_meta_val->type =
+      type::Fn::with_args({type::mirror_type, type::symbol_type});
   set_meta_val = new GenericFn;
-  set_meta_val->type = type::Fn::with_args({type::mirror_type, type::symbol_type, new type::Any});
+  set_meta_val->type = type::Fn::with_args(
+      {type::mirror_type, type::symbol_type, new type::Any});
   delete_meta = new GenericFn;
-  delete_meta->type = type::Fn::with_args({type::mirror_type, type::symbol_type});
+  delete_meta->type =
+      type::Fn::with_args({type::mirror_type, type::symbol_type});
 
   get_mirror = new GenericFn;
   get_mirror->type = type::Fn::with_args({new type::Any});
 
   get_mirror->add(new InbuiltOperator(
-                                      "reflect", "gets a mirror of the target object",
-                                      op_get_mirror,
-                                      type::Fn::with_args({new type::Any}),  true));
+      "reflect", "gets a mirror of the target object", op_get_mirror,
+      type::Fn::with_args({new type::Any}), true));
 
   get_slots->add(new InbuiltOperator(
       "get-slots", "gets a list of slot symbols from a mirror", op_get_slots,
@@ -138,16 +145,13 @@ void op::initialize_mirror() {
 
   set_slot_val->add(new InbuiltOperator(
       "set-slot", "sets the value of a given slot", op_set_slot_val,
-      type::Fn::with_args({type::mirror_type, type::symbol_type, new type::Any}),
+      type::Fn::with_args(
+          {type::mirror_type, type::symbol_type, new type::Any}),
       true));
 
   delete_slot->add(new InbuiltOperator(
       "remove-slot", "removes a slot from an object", op_delete_slot,
       type::Fn::with_args({type::mirror_type, type::symbol_type}), true));
-
-
-
-
 
   get_meta->add(new InbuiltOperator(
       "get-metadata", "gets the list of all metadata slots from a mirror",
@@ -161,7 +165,8 @@ void op::initialize_mirror() {
   set_meta_val->add(new InbuiltOperator(
       "set-metaslot", "sets the value of a given metadata slot",
       op_set_meta_val,
-      type::Fn::with_args({type::mirror_type, type::symbol_type, new type::Any}),
+      type::Fn::with_args(
+          {type::mirror_type, type::symbol_type, new type::Any}),
       true));
 
   delete_meta->add(new InbuiltOperator(
